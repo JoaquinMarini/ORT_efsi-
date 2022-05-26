d9a6b8e1c6b87ea9e767f4aea809baf10c2bc38a
JoaquinMarini <101258852+JoaquinMarini@users.noreply.github.com>
2022-05-26 12:13:07 -0300

    TP2 Terminado :) 26/5

--- EFSI-Jeesssik-main/TP2EFSI/main.js
+++ EFSI-Jeesssik-main/TP2EFSI/main.js
@@ -7,13 +7,19 @@ function prueba(){
             checked:false,
             text:texto.value
         })
+    }
         toDoHTML.innerHTML=""
         for(i in toDo){
-            toDoHTML.innerHTML+='<br><input type="checkbox" class="form-check-input">'+toDo[i].text
+            if(toDo[i].checked){
+                toDoHTML.innerHTML+=`<br><input type="checkbox" class="form-check-input" id="${i}" onclick="done(${i})" checked><s style="color:grey;">${toDo[i].text}</s>`
+            }else{
+                toDoHTML.innerHTML+=`<br><input type="checkbox" class="form-check-input" id="${i}" onclick="done(${i})">`+toDo[i].text
+            }
         }
-        console.log(toDo.length)
-        console.log(toDo[toDo.length-1].text)
-    }
     texto.value=""
+}
+function done(i){
+    toDo[i].checked=!toDo[i].checked;
+    prueba();
 }
 
